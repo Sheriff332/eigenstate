d996ba68bfeca6e55524a8323d8a2071bb7b4d62
dotstate <dotstate@localhost>
2026-06-10 15:45:26 +0530

    Add 1 file, Update 3 files, Remove 1 file: config, hyprland.lua, kitty.conf, gamemode.sh, powerdog.sh

--- Personal/.config/hypr/hyprland.lua
+++ Personal/.config/hypr/hyprland.lua
@@ -32,7 +32,7 @@ hl.env("XDG_SESSION_DESKTOP", "Hyprland")
 ------------------
 hl.monitor({
     output = "",
-    mode = "1920x1080@144",
+    mode = "1920x1080@60",
     position = "auto",
     scale = "1"
 })
@@ -51,7 +51,6 @@ hl.on("hyprland.start", function ()
     hl.exec_cmd("gsr-ui")
     hl.exec_cmd("brightnessctl -d 'platform::micmute' set $(wpctl get-volume @DEFAULT_AUDIO_SOURCE@ | grep -q 'MUTED' && echo 1 || echo 0)")
     hl.exec_cmd("hyprctl setcursor Bibata-Modern-Ice 24")
-    hl.exec_cmd("sudo /home/yourusername/.local/bin/powerdog.sh")
 end)
 
 -----------------
